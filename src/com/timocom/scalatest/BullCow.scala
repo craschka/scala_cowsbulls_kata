c79e069c545b26abcee85118afc33f528903ac1b
Christian Raschka <christian.raschka@gmail.com>
2012-07-25 12:55:05 +0200

    initial commit

---
+++ src/com/timocom/scalatest/BullCow.scala
@@ -0,0 +1,56 @@
+package com.timocom.scalatest
+
+import util.Random
+import collection.immutable.IndexedSeq
+
+object BullCow {
+  def main(args: Array[String]): Unit = {
+
+    val game = new Game
+    game.start()
+  }
+}
+
+class Game {
+  val number = randomNumber()
+
+  def start() {
+
+    print("Guess a 4-digit number with no duplicate digits: ")
+
+    val guesses = evaluate(0, 0)
+    println("You won after "+guesses+" guesses!")
+    println("number was " +number)
+  }
+
+
+  def evaluate(_bulls: Int, guesses: Int): Int = {
+    if (_bulls==4){
+      return guesses
+    }
+    val input = Console.readInt()
+    val cows = calcCows(input, number)
+    val bulls = calcBulls(input, number)
+    println("%d Cows and %d Bulls.".format(cows, bulls))
+
+    evaluate(bulls,guesses + 1)
+  }
+
+  def calcBulls(input: Int, number: Int): Int = {
+    input.toString.map(a => if (number.toString.charAt(input.toString.indexOf(a)) == a) 1 else 0).foldRight(0)((x, accu) => x + accu)
+  }
+
+  def calcCows(input: Int, number: Int): Int = {
+    input.toString.map(a => if (number.toString.contains(a)) 1 else 0).foldRight(0)((x, accu) => x + accu)
+  }
+
+  def randomNumber(): Int = {
+    var list = "" + randomDigit + randomDigit + randomDigit + randomDigit
+    if (list.distinct.length != list.length) list = randomNumber().toString
+    list.toInt
+  }
+
+  def randomDigit: Int = {
+    Random.nextInt(9) + 1
+  }
+}
---
+++ test/com/timocom/scalatest/BullCowTest.scala
@@ -0,0 +1,42 @@
+package com.timocom.scalatest
+
+import org.scalatest.{OneInstancePerTest, FlatSpec}
+import org.scalatest.matchers.ShouldMatchers
+
+class BullCowTest extends FlatSpec with ShouldMatchers with OneInstancePerTest {
+
+  val game = new Game
+
+  "RandomGenerator" should "generate 4 numbers" in {
+    game.randomNumber().toString.length should be (4)
+  }
+
+  it should "not have same digits" in {
+    val number = game.randomNumber()
+    number.toString.distinct.length should be (number.toString.length)
+  }
+
+  it should "be distinct with every call" in {
+    val number = game.randomNumber()
+    val next = game.randomNumber()
+
+    number should not be (next)
+  }
+
+  "cows" should "be the contains count of the input" in {
+    game.calcCows(1234,4321) should be (4)
+    game.calcCows(1234,8165) should be (1)
+    game.calcCows(1234,4367) should be (2)
+    game.calcCows(1234,2348) should be (3)
+    game.calcCows(1234,5678) should be (0)
+  }
+
+  "bulls" should "be the exact number of hits" in {
+    game.calcBulls(1234,1423) should be (1)
+    game.calcBulls(1234,4321) should be (0)
+    game.calcBulls(1234,1324) should be (2)
+    game.calcBulls(1234,1238) should be (3)
+    game.calcBulls(1234,1234) should be (4)
+    game.calcBulls(8431,8888)
+  }
+}
